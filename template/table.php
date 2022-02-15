ac2de1da3dc5464ffc2af1989a6fd40c3d314bfd
Gading Sobad <gading.rengga@gmail.com>
2022-02-15 12:55:11 +0700

    Update search Button

--- template/table.php
+++ template/table.php
@@ -83,28 +83,32 @@ class create_table
 							?>
 						</span>
 					</div> -->
-						<div class="col-md-7 pl-0 ml-0 pr-0 text-right">
-							<div class="sasi-search">
-								<div class="row m-0">
-									<div class="input-group form-search">
-										<div class="col-sm-9 p-0">
-											<input type="text" name="words<?php print($search); ?>" class="form-control sasi-form" data-sobad="<?php print($func); ?>" data-load="<?php print($load); ?>" data-type="<?php print($type); ?>" data-object="<?php print($obj); ?>" value="<?php print($value); ?>" placeholder="Penelusuran">
-										</div>
-										<div class="col-sm-3 text-right p-0">
-											<button type="button" data-sobad="<?php print($func); ?>" class="btn sasi-btn-search" aria-expanded="false" data-load="<?php print($load); ?>" data-object="<?php print($obj); ?>" onclick="sobad_search(this)" data-type="<?php print($type); ?>" data-load="<?php print($load); ?>" value="<?php print($value); ?>">
-												<i class="fa fa-search" aria-hidden="true"></i>
-											</button>
+						<div class="sasi-search-content">
+							<div class="col-md-7 pl-0 ml-0 pr-0 text-right">
+								<div class="sasi-search">
+									<div class="row m-0">
+										<div class="input-group form-search">
+											<div class="col-sm-9 p-0">
+												<input type="text" name="words<?php print($search); ?>" class="form-control sasi-form" data-sobad="<?php print($func); ?>" data-load="<?php print($load); ?>" data-type="<?php print($type); ?>" data-object="<?php print($obj); ?>" value="<?php print($value); ?>" placeholder="Penelusuran">
+											</div>
+											<div class="col-sm-3 text-right p-0">
+												<button type="button" data-sobad="<?php print($func); ?>" class="btn sasi-btn-search" aria-expanded="false" data-load="<?php print($load); ?>" data-object="<?php print($obj); ?>" onclick="sobad_search(this)" data-type="<?php print($type); ?>" data-load="<?php print($load); ?>" value="<?php print($value); ?>">
+													<i class="fa fa-search" aria-hidden="true"></i>
+												</button>
+											</div>
 										</div>
 									</div>
-								</div>
-							</div><!-- /input-group -->
+								</div><!-- /input-group -->
+							</div>
 						</div>
-						<div class="col-md-4 p-0">
-							<?php
-							if (!empty($check)) {
-								self::_dropdown($args, $search, $val_src);
-							}
-							?>
+						<div class="sasi-search-filter">
+							<div class="col-md-4 p-0">
+								<?php
+								if (!empty($check)) {
+									self::_dropdown($args, $search, $val_src);
+								}
+								?>
+							</div>
 						</div>
 					</div>
 				</form>
