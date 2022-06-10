f62b60598b0dfc209bcddc3917df7b083f3bda31
Gading Sobad <gading.rengga@gmail.com>
2022-06-10 11:15:05 +0700

    Update js nav-dropdown

--- scripts.php
+++ scripts.php
@@ -1,95 +1,103 @@
 <?php
-(!defined('THEMEPATH'))?exit:'';
+(!defined('THEMEPATH')) ? exit : '';
 
-class theme_script{
-	public function _get_($func='',$idx=array()){
-		if(is_callable(array($this,$func))){
+class theme_script
+{
+	public function _get_($func = '', $idx = array())
+	{
+		if (is_callable(array($this, $func))) {
 			$script = self::$func($idx);
-		}else{
+		} else {
 			$script = array();
 		}
-		
+
 		return $script;
 	}
-	
-	private function lokasi(){
-		return 'theme/'._theme_folder.'/asset/';
+
+	private function lokasi()
+	{
+		return 'theme/' . _theme_folder . '/asset/';
 	}
-// BEGIN PAGE LEVEL STYLES ---->
-	
-	private function _css_page_level($idx=array()){
+	// BEGIN PAGE LEVEL STYLES ---->
+
+	private function _css_page_level($idx = array())
+	{
 		$loc = $this->lokasi();
 		$css = array(
-			'themes-login-soft'	=> $loc.'css/sasi/sasi-login.css',
-			'themes-search'		=> $loc.'css/pages/search.css'
+			'themes-login-soft'	=> $loc . 'css/sasi/sasi-login.css',
+			'themes-search'		=> $loc . 'css/pages/search.css'
 		);
-		
+
 		$check = array_filter($idx);
-		if(!empty($check)){
-			foreach($idx as $key){
+		if (!empty($check)) {
+			foreach ($idx as $key) {
 				$css[$key];
 			}
 		}
-		
-		return $css;
-	}
-	
-	
-// BEGIN PAGE STYLES ---->	
-	private function _css_page($idx=array()){
-		$loc = $this->lokasi();
-		$css = array(
-			'component'			=> $loc.'css/pages/components.css',
-			'sasi-component'	=> $loc.'css/sasi/sasi-component.css',
-		);
-		
-		$check = array_filter($idx);
-		if(!empty($check)){
-			foreach($idx as $key){
-				$css[$key];
-			}
-		}
-		
-		return $css;
-	}
-	
-// BEGIN THEME STYLES ---->
-	private function _css_theme($idx=array()){
-		$loc = $this->lokasi();
-		$css = array(
-			'themes-bootstrap'		=> $loc.'css/sasi/sasi-bootstrap.css',
-			'themes-primary'		=> $loc.'css/sasi/sasi-ui.css',
-			'themes-icon'			=> $loc.'css/sasi/sasi-icon.css',
-		);
-		
-		$check = array_filter($idx);
-		if(!empty($check)){
-			foreach($idx as $key){
-				$js[$key];
-			}
-		}
-		
+
 		return $css;
 	}
 
-// BEGIN PAGE LEVEL SCRIPTS ---->
-	private function _js_page_level($idx=array()){
+
+	// BEGIN PAGE STYLES ---->	
+	private function _css_page($idx = array())
+	{
 		$loc = $this->lokasi();
-		$js = array( 
-//			'themes-sasi'			=> $loc.'js/sasi/sasi.js',
-			'themes-login-soft'		=> $loc.'js/pages/login-soft.js',
-			'themes-ui-toastr'		=> $loc.'js/pages/ui-toastr.js',
-			'themes-dropdown'		=> $loc.'js/pages/components-dropdowns.js',
-			'themes-editor'			=> $loc.'js/pages/components-editors.js',
+		$css = array(
+			'component'			=> $loc . 'css/pages/components.css',
+			'sasi-component'	=> $loc . 'css/sasi/sasi-component.css',
 		);
-		
+
 		$check = array_filter($idx);
-		if(!empty($check)){
-			foreach($idx as $key){
+		if (!empty($check)) {
+			foreach ($idx as $key) {
+				$css[$key];
+			}
+		}
+
+		return $css;
+	}
+
+	// BEGIN THEME STYLES ---->
+	private function _css_theme($idx = array())
+	{
+		$loc = $this->lokasi();
+		$css = array(
+			'themes-bootstrap'		=> $loc . 'css/sasi/sasi-bootstrap.css',
+			'themes-primary'		=> $loc . 'css/sasi/sasi-ui.css',
+			'themes-icon'			=> $loc . 'css/sasi/sasi-icon.css',
+		);
+
+		$check = array_filter($idx);
+		if (!empty($check)) {
+			foreach ($idx as $key) {
 				$js[$key];
 			}
 		}
-		
+
+		return $css;
+	}
+
+	// BEGIN PAGE LEVEL SCRIPTS ---->
+	private function _js_page_level($idx = array())
+	{
+		$loc = $this->lokasi();
+		$js = array(
+			//			'themes-sasi'			=> $loc.'js/sasi/sasi.js',
+			'themes-login-soft'		=> $loc . 'js/pages/login-soft.js',
+			'themes-ui-toastr'		=> $loc . 'js/pages/ui-toastr.js',
+			'themes-dropdown'		=> $loc . 'js/pages/components-dropdowns.js',
+			'themes-editor'			=> $loc . 'js/pages/components-editors.js',
+			'themes-dropdown-nav'	=> $loc . 'js/pages/dropdown-nav.js',
+		);
+
+		$check = array_filter($idx);
+		if (!empty($check)) {
+			foreach ($idx as $key) {
+				$js[$key];
+			}
+		}
+
 		return $js;
-	}	
+	}
 }
