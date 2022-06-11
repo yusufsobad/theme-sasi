65b2b43ab66604427bfd1789fa7bbf3288e3fc1d
Gading Sobad <gading.rengga@gmail.com>
2022-06-11 10:01:42 +0700

    Update New Tab

--- asset/js/pages/newtab.js
+++ asset/js/pages/newtab.js
@@ -18,4 +18,25 @@ $(function () {
     $('.context-menu-one').on('click', function (e) {
         console.log('clicked', this);
     })
+
+    $.contextMenu({
+        selector: '.sasi_childmenu',
+        callback: function (key, opt) {
+            if (key == 'New Tab') {
+                sobad_windows(opt.$trigger.attr("data-uri"));
+            }
+        },
+        items: {
+            "New Tab": {
+                name: "New Tab", icon: function () {
+                    return '';
+                }
+            }
+        }
+    });
+
+    $('.context-menu-one').on('click', function (e) {
+        console.log('clicked', this);
+    })
+
 });
