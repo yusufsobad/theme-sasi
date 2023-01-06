728142c0464be4fddcd124a9b7f76476a6307c73
gadingsobad <gading.soloabadi@gmail.com>
2023-01-06 09:02:50 +0700

    Update Accordion Table

--- template/table.php
+++ template/table.php
@@ -320,9 +320,11 @@ class create_table
 
 				if ($acordion) {
 
-					echo '<tr style="order-top: 10px solid #f5f5f5; border-bottom: 10px solid #f5f5f5;" class="accordian-body collapse"  id="' . $config_accordion['ID'] . '"><td colspan="100%">';
+					echo '<tr style="border-top: 10px solid #f5f5f5; border-bottom: 10px solid #f5f5f5;" class="accordian-body collapse"  id="' . $config_accordion['ID'] . '">';
+					echo '<td colspan="100%">';
 					echo '<div class="loader"></div>';
-					echo '	</td></tr>';
+					echo '</td>';
+					echo '</tr>';
 				}
 			}
 
