1e3f5ee946012bf6aa5f25b54f6aba6ec668d7ea
itaq <anggitasw5@gmail.com>
2023-02-01 09:07:40 +0700

    Fix Bug Grid system

--- template/grid.php
+++ template/grid.php
@@ -1,12 +1,11 @@
 <?php
-(!defined('THEMEPATH')) ? exit : '';
+!defined('THEMEPATH') ? exit() : '';
 
 class sobad_grid
 {
-
-    public static function create_grid($data=array())
+    public static function create_grid($data = [])
     {
-    ?>
+        ?>
         <div class="p-lg">
             <div class="row">
                 <?php foreach ($data as $key => $val) {
@@ -17,20 +16,19 @@ class sobad_grid
     <?php
     }
 
-   private static function grid($data=array())
+    private static function grid($data = [])
     {
         $id = isset($data['id']) ? $data['id'] : '';
         $col = isset($data['col']) ? $data['col'] : 12;
         ?>
-            <div id="<?= $id; ?>" class="col-md-<?= $col; ?>">
+            <div id="<?= $id ?>" class="col-md-<?= $col ?>">
                 <?php
-                    $func = isset($val['func']) ? $val['func'] : '';
-                    if (method_exists('sasi_template', $func)) {
-                        sasi_template::{$func}($val['data']);
-                    }else{
-                        self::create_grid($val['data']);
-                    }
-                ?> 
+                $func = isset($data['func']) ? $data['func'] : '';
+                if (method_exists('sasi_template', $func)) {
+                    sasi_template::{$func}($data['data']);
+                } else {
+                    self::create_grid($data['data']);
+                }?> 
             </div>
         <?php
     }
